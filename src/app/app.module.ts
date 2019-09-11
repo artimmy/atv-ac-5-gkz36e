99848d0432bd48a5b5bebcb42613d80da35d1005
artimmy <arthurtimmy@hotmail.com>
2019-09-11 09:21:48 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,21 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { TabelaComponent } from './tabela/tabela.component';
 import { ServicoService } from './servico.service';
-
-
+// para usar a moeda
+import {LOCALE_ID} from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localePt);
+// ^^^^^
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, FormComponent, TabelaComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [ServicoService]
+  providers: [ServicoService,
+  /* alem de importar aquelas bibliotecas preciso colocar essas duas linhas dentro de providers junto com o servico */
+  {
+      provide: LOCALE_ID,
+      useValue: "pt-BR"
+    }]
 })
 export class AppModule { }
